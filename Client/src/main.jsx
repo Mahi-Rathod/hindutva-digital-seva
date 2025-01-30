c329fd998432322a24b7d3b896267fbfafb89819
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2025-01-30 19:57:19 +0530

    added vercel analytics

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
 import Store from "./redux/Store.js";
-
+import { inject } from '@vercel/analytics';
+inject();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
